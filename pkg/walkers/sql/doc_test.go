aed28391c2960cfd01dd0a3c64142096fdd8c213
yaacov <kobi.zamir@gmail.com>
2018-12-02 20:02:16 +0200

    update docs for walkers

--- pkg/walkers/sql/doc_test.go
+++ pkg/walkers/sql/doc_test.go
@@ -13,7 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package sql helps to create SQL squirrel filters using the TSL package.
 package sql
 
 import (
--- pkg/walkers/sql/walk.go
+++ pkg/walkers/sql/walk.go
@@ -13,6 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package sql helps to create SQL squirrel filters using the TSL package.
 package sql
 
 import (
